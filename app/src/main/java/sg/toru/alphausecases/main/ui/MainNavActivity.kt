b79af54ca38efc7128131e45d2ae446058660786
Toru Choi <blueocean239@gmail.com>
2020-10-26 18:49:48 +0800

    add navigation

---
+++ app/src/main/java/sg/toru/alphausecases/main/ui/MainNavActivity.kt
@@ -0,0 +1,12 @@
+package sg.toru.alphausecases.main.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sg.toru.alphausecases.R
+
+class MainNavActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_nav)
+    }
+}
